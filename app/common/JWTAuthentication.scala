c9de12f78c1746f3ef5f61062846e27a327d9d14
zhengzhicong <zhengzhicong@growingio.com>
2022-09-20 23:21:59 +0800

    :tada: add jwt

--- app/common/JWTAuthentication.scala
+++ app/common/JWTAuthentication.scala
@@ -3,28 +3,28 @@ package common
 import auth.domain.User
 import play.api.libs.json.Json
 import play.api.mvc.Results.{Ok, Unauthorized}
-import play.api.mvc.{ActionBuilder, AnyContent, BodyParser, Request, Result, WrappedRequest}
+import play.api.mvc.{ActionBuilder, AnyContent, BodyParser, BodyParsers, Request, Result, WrappedRequest}
 import utils.Jwt
 
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 case class UserRequest[A](user: User, request: Request[A]) extends WrappedRequest(request)
 
-object JWTAuthentication extends ActionBuilder[UserRequest, AnyContent] {
+class JWTAuthentication @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+  extends ActionBuilder[UserRequest, AnyContent] {
 
   def invokeBlock[A](request: Request[A], block: UserRequest[A] => Future[Result]): Future[Result] = {
 
-    implicit val req = request
-
-    val jwtToken = request.headers.get("jw_token").getOrElse("")
+    val jwtToken: String = request.headers.get("jw_token").getOrElse("")
 
     if (Jwt.isValidToken(jwtToken)) {
-      Jwt.decodePayload(jwtToken).fold(Future.successful(Unauthorized("Invalid credential"))) { payload =>
+      Jwt.decodePayload(jwtToken).fold(Future.successful(Unauthorized("Invalid credential"))) { payload: String =>
 
         val userId: Long = Json.parse(payload).validate[Long].get
 
         // Replace this block with data source
-        if (true) {
+        if (userId > 1) {
           Future.successful(Ok("Authorization successful"))
         } else {
           Future.successful(Unauthorized("Invalid credential"))
@@ -35,7 +35,7 @@ object JWTAuthentication extends ActionBuilder[UserRequest, AnyContent] {
     }
   }
 
-  override def parser: BodyParser[AnyContent] = ???
+  override def parser: BodyParser[AnyContent] = parser
 
-  override protected def executionContext: ExecutionContext = ???
+  override protected def executionContext: ExecutionContext = ec
 }
