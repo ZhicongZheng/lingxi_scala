5f38485dc9d0ddbac96b5f339df082b551b9e36c
zhengzzv <1064275075@qq.com>
2022-10-09 11:11:24 +0800

    fix by empty request body

--- app/auth/controller/AuthWriteController.scala
+++ app/auth/controller/AuthWriteController.scala
@@ -6,10 +6,11 @@ import common.ResultHelper
 import common.exceptions.BizException
 import play.api.http.HeaderNames
 import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, ControllerComponents, InjectedController, Request}
+import play.api.mvc._
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class AuthWriteController @Inject()(override val controllerComponents: ControllerComponents,
@@ -17,12 +18,14 @@ class AuthWriteController @Inject()(override val controllerComponents: Controlle
   extends InjectedController {
 
   def login: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val loginRequest = Json.fromJson[LoginRequest](request.body.asJson.get)
-    authApplicationService.login(loginRequest.get).map { token =>
-      Ok(ResultHelper.success(None)).withHeaders((HeaderNames.AUTHORIZATION, token))
-    }.recover { case ex: BizException =>
-      Ok(ResultHelper.fail(ex.getResponseCode))
-    }
+    request.body.asJson.map { json =>
+      val loginRequest = Json.fromJson[LoginRequest](json)
+      authApplicationService.login(loginRequest.get).map { token =>
+        Ok(ResultHelper.success(None)).withHeaders((HeaderNames.AUTHORIZATION, token))
+      }.recover { case ex: BizException =>
+        Ok(ResultHelper.fail(ex.getResponseCode))
+      }
+    } getOrElse Future.successful(BadRequest("Expecting Json data"))
   }
 
 }
