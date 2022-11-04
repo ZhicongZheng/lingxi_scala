4e7cb2e1da006ef2a47a9b0d55080f7c22a34986
zhengzzv <1064275075@qq.com>
2022-11-04 17:44:33 +0800

    :sparkles: 超级管理员不鉴权

--- app/common/actions/AuthorizationAction.scala
+++ app/common/actions/AuthorizationAction.scala
@@ -4,6 +4,7 @@ import auth.domain.repository.{PermissionRepository, RoleRepository, UserReposit
 import common.Constant
 import common.actions.AuthorizedRequest.{failureResult, parsePermission}
 import common.result.PERMISSION_DENIED
+import common.Constant.superAdmin
 import play.api.mvc.{ActionRefiner, BodyParsers, Result, WrappedRequest}
 import play.api.routing.{HandlerDef, Router}
 
@@ -29,14 +30,22 @@ class AuthorizationAction @Inject() (
       role        <- roleRepository.findByUserId(user.id)
       permissions <- if (role.isEmpty) Future.successful(Nil) else permissionRepository.findByRoleId(role.get.id)
     } yield {
-      val handlerDef = userRequest.request.attrs.get(Router.Attrs.HandlerDef)
-      val permission = parsePermission(handlerDef)
-      permissions.filter(_.`type` == Constant.functionPermission).filter(_.value == permission) match {
-        case Nil => Left(failureResult)
-        case _   => Right(AuthorizedRequest(userRequest.copy(user = user.copy(role = role, permissions = permissions))))
+      val handlerDef        = userRequest.request.attrs.get(Router.Attrs.HandlerDef)
+      val permission        = parsePermission(handlerDef)
+      def authorizedRequest = AuthorizedRequest(userRequest.copy(user = user.copy(role = role, permissions = permissions)))
+      role match {
+        case None => Left(failureResult)
+        // 超级管理员不校验权限
+        case Some(role) if role.code == superAdmin => Right(authorizedRequest)
+        case Some(_) =>
+          permissions.filter(_.`type` == Constant.functionPermission).filter(_.value == permission) match {
+            case Nil => Left(failureResult)
+            case _   => Right(authorizedRequest)
+          }
       }
     }
   }
+
 }
 
 object AuthorizedRequest {
