250e7d0be8a85d4076a4e5a8096c2f4096b01947
zhengzzv <1064275075@qq.com>
2023-01-06 16:34:29 +0800

    认证改造，使用cookie

---
+++ app/infra/actions/AuthenticationAction.scala
@@ -0,0 +1,56 @@
+package infra.actions
+
+import common.{Constant, TOKEN_CHECK_ERROR}
+import domain.user.entity.User
+import infra.actions.AuthenticationAction.{noAuthRoute, tokenValidateError}
+import play.api.libs.json.Json
+import play.api.mvc._
+import play.api.Logging
+
+import java.util.regex.Pattern
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+case class UserRequest[A](user: User, request: Request[A]) extends WrappedRequest(request)
+
+@Singleton
+class AuthenticationAction @Inject()(parser: BodyParsers.Default)(implicit
+                                                                  ec: ExecutionContext
+) extends ActionBuilder[UserRequest, AnyContent]
+    with Logging {
+
+  def invokeBlock[A](request: Request[A], block: UserRequest[A] => Future[Result]): Future[Result] = {
+
+    def success(user: User) = block.apply(UserRequest(user, request))
+
+    implicit val path: String = request.path
+    if ("/" == path || noAuthRoute.exists(p => p.matcher(path).find())) {
+      return success(null)
+    }
+
+    request.session.get(Constant.SESSION_USER) match {
+      case Some(sessionUser) => success(Json.fromJson[User](Json.parse(sessionUser)).get)
+      case None              => tokenValidateError
+    }
+
+  }
+
+  override def parser: BodyParser[AnyContent] = parser
+
+  override protected def executionContext: ExecutionContext = ec
+}
+
+object AuthenticationAction extends Logging {
+
+  val noAuthRoute: Seq[Pattern] = Seq(
+    Pattern.compile("/users/login"),
+    Pattern.compile("/users/logout"),
+    Pattern.compile("/docs/*"),
+    Pattern.compile("/*.ico")
+  )
+
+  private def tokenValidateError(implicit path: String) = {
+    logger.info(s"request path : $path, jwt token in cookie validate fail")
+    Future.successful(common.Results.fail(TOKEN_CHECK_ERROR))
+  }
+}
--- app/interfaces/controller/RoleController.scala
+++ app/interfaces/controller/RoleController.scala
@@ -1,9 +1,9 @@
 package interfaces.controller
 
 import application.command.{CreateRoleCommand, UpdateRoleCommand}
-import application.service.{RoleCommandService, RoleQueryService, UserQueryService}
+import application.service.{RoleCommandService, RoleQueryService}
 import common.{Page, PageQuery, Results}
-import infra.actions.{AuthorizationAction, UserAction}
+import infra.actions.{AuthenticationAction, AuthorizationAction}
 import interfaces.dto.{PermissionDto, RoleDto}
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.{ControllerComponents, InjectedController}
@@ -16,46 +16,50 @@ class RoleController @Inject() (
   override val controllerComponents: ControllerComponents,
   roleQueryService: RoleQueryService,
   roleCommandService: RoleCommandService,
-  userAction: UserAction,
+  authenticationAction: AuthenticationAction,
   authorizationAction: AuthorizationAction
 ) extends InjectedController {
 
-  def listRoleByPage(page: Int, size: Int, sort: Option[String] = None) = userAction andThen authorizationAction async {
-    implicit val roleFormat: OFormat[Page[RoleDto]] = Json.format[Page[RoleDto]]
-    val pageQuery                                   = PageQuery(page, size, sort)
-    roleQueryService.listRolesByPage(pageQuery).map(pageDto => Results.success(pageDto)).recover(ex => Results.fail(ex))
-  }
+  def listRoleByPage(page: Int, size: Int, sort: Option[String] = None) =
+    authenticationAction andThen authorizationAction async {
+      implicit val roleFormat: OFormat[Page[RoleDto]] = Json.format[Page[RoleDto]]
+      val pageQuery                                   = PageQuery(page, size, sort)
+      roleQueryService.listRolesByPage(pageQuery).map(pageDto => Results.success(pageDto)).recover(ex => Results.fail(ex))
+    }
 
-  def changeUserRole(userId: Long, roleId: Long) = userAction andThen authorizationAction async {
-    roleCommandService.changeUserRole(userId, roleId) map {
-      case Left(error) => Results.fail(error)
-      case Right(_)    => Ok
-    } recover (ex => Results.fail(ex))
-  }
-
-  def createRole = userAction(parse.json[CreateRoleCommand]) andThen authorizationAction async { request =>
-    roleCommandService
-      .createRole(request.body)
-      .map {
-        case Left(error) => Results.fail(error)
-        case Right(id)   => Created(Json.toJson(id))
-      }
-      .recover(ex => Results.fail(ex))
-  }
-
-  def updateRole = userAction(parse.json[UpdateRoleCommand]) andThen authorizationAction async { request =>
-    val updater           = request.user.id
-    val updateRoleRequest = request.body.copy(updateBy = Some(updater))
-    roleCommandService
-      .updateRole(updateRoleRequest)
-      .map {
+  def changeUserRole(userId: Long, roleId: Long) =
+    authenticationAction andThen authorizationAction async {
+      roleCommandService.changeUserRole(userId, roleId) map {
         case Left(error) => Results.fail(error)
         case Right(_)    => Ok
-      }
-      .recover(ex => Results.fail(ex))
-  }
+      } recover (ex => Results.fail(ex))
+    }
 
-  def deleteRole(id: Int) = userAction andThen authorizationAction async {
+  def createRole =
+    authenticationAction(parse.json[CreateRoleCommand]) andThen authorizationAction async { request =>
+      roleCommandService
+        .createRole(request.body)
+        .map {
+          case Left(error) => Results.fail(error)
+          case Right(id)   => Created(Json.toJson(id))
+        }
+        .recover(ex => Results.fail(ex))
+    }
+
+  def updateRole =
+    authenticationAction(parse.json[UpdateRoleCommand]) andThen authorizationAction async { request =>
+      val updater           = request.user.id
+      val updateRoleRequest = request.body.copy(updateBy = Some(updater))
+      roleCommandService
+        .updateRole(updateRoleRequest)
+        .map {
+          case Left(error) => Results.fail(error)
+          case Right(_)    => Ok
+        }
+        .recover(ex => Results.fail(ex))
+    }
+
+  def deleteRole(id: Int) = authenticationAction andThen authorizationAction async {
     roleCommandService
       .deleteRole(id)
       .map {
@@ -65,7 +69,7 @@ class RoleController @Inject() (
       .recover(ex => Results.fail(ex))
   }
 
-  def listPermission() = userAction andThen authorizationAction async {
+  def listPermission() = authenticationAction andThen authorizationAction async {
     roleQueryService
       .listPermission()
       .map(ps => ps.map(PermissionDto.fromPo))
