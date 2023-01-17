18155b9c0fa8dacbc334c2f92dac90301a533954
zhengzzv <1064275075@qq.com>
2023-01-17 20:55:09 +0800

    test

--- app/infra/db/repository/impl/ActionRepositoryImpl.scala
+++ app/infra/db/repository/impl/ActionRepositoryImpl.scala
@@ -7,6 +7,9 @@ import scala.concurrent.Future
 class ActionRepositoryImpl extends ActionRepository {
   override def save(domain: Action): Future[Long] = ???
 
+
+
+
   override def get(id: Long): Future[Option[Action]] = ???
 
   override def remove(id: Long): Future[Unit] = ???
