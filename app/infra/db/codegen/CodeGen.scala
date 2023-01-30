5aa96132a667f8fc3158e21f63857ed6dd44fb35
zhengzzv <37789928+zhengzzv@users.noreply.github.com>
2023-01-30 11:30:18 +0800

    Update CodeGen.scala

--- app/infra/db/codegen/CodeGen.scala
+++ app/infra/db/codegen/CodeGen.scala
@@ -2,7 +2,7 @@ package infra.db.codegen
 object CodeGen {
   def main(args: Array[String]): Unit = {
     slick.codegen.SourceCodeGenerator.main(
-      Array("slick.jdbc.PostgresProfile", "org.postgresql.Driver", "jdbc:postgresql://43.143.185.11:5454/lingxi", "/Users/moka/lingxi/lingxi_scala/app/infra/db/codegen", "infra.db.codegen", "root", "i1gcoyCMzkPYNCKHIOwL")
+      Array("slick.jdbc.PostgresProfile", "org.postgresql.Driver", "", "/Users/moka/lingxi/lingxi_scala/app/infra/db/codegen", "infra.db.codegen", "root", "")
     )
 
   }
